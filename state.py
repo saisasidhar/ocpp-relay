158f7c190419e44bbb2f279a4dd853fceb58db25
Sai Sasidhar Maddali <mail@sasidhar.in>
2025-06-08 22:51:40 -0700

    feature: persist state using redis across streamlit sessions
      One of the challenge in previous version of the application is that
      when a browser tab is closed and re-opened, streamlit session used
      to begin from scratch. However, the relay is already locked and
      loaded. To solve this problem, AppState is persisted using a redis.
      Originally tried sqlite3 in memory but ran into multithreading issues.
    
      Attributes in AppState are implemented using a "RedisBacked"
      python descriptor

--- state.py
+++ state.py
@@ -6,6 +6,7 @@ import time
 import uuid
 from typing import Dict, Optional
 
+import redis
 import streamlit as st
 
 
@@ -17,21 +18,60 @@ class Event:
     response: Optional[str] = None
 
 
-@dataclasses.dataclass
+_redis = redis.Redis(decode_responses=True)
+
+
+class RedisBackedAttr:
+    def __init__(self, key, data_type, default=None):
+        self.key = f"ocpp-relay:{key}"
+        self.data_type = data_type
+        self.default = default
+
+    def __get__(self, instance, owner):
+        val = _redis.get(self.key)
+        # if val is None and self.default is not None:
+        #     self.__set__(instance, self.default)
+        #     return self.default
+        return eval(val) if val is not None else self.default
+
+    def __set__(self, instance, value):
+        current = _redis.get(self.key)
+        if current is None or eval(current) != value:
+            _redis.set(self.key, repr(value))
+
+
 class AppState:
     """..."""
 
-    csms_info: str = ""
-    relay_configured: bool = False
-    relay_connected: bool = False
-    relay_url: str = "ws://localhost:8500"
+    csms_info: str = RedisBackedAttr("csms_info", data_type=str, default="")
+    relay_configured: bool = RedisBackedAttr(
+        "relay_configured", data_type=bool, default=False
+    )
+    relay_connected: bool = RedisBackedAttr(
+        "relay_connected", data_type=bool, default=False
+    )
+    relay_url: str = RedisBackedAttr(
+        "relay_url", data_type=str, default="ws://localhost:8500"
+    )
+    charge_point_id: Optional[str] = RedisBackedAttr(
+        "charge_point_id", data_type=str, default=""
+    )
+    latest_event: Optional[str] = RedisBackedAttr(
+        "latest_event", data_type=str, default=""
+    )
     relay_connection_manager = None
-    charge_point_id: Optional[str] = ""
-    latest_event: Optional[str] = ""
     events = collections.OrderedDict()
     injected_message_ids = []
+    _instance = None
+
+    @classmethod
+    def instantiate(cls):
+        # singleton
+        if cls._instance is None:
+            cls._instance = cls()
+        return cls._instance
 
 
 def introduce_statefulness():
     if "app_state" not in st.session_state:
-        st.session_state.app_state = AppState()
+        st.session_state.app_state = AppState.instantiate()
